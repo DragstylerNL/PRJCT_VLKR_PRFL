df4e5e384878eb4a45475d8914a085b63c92cb1d
DragstylerNL <dragstylernl@gmail.com>
2020-03-31 21:28:44 +0200

    URP for them GUD SpriteGraphics

---
+++ VLKR_PRFL/Assets/_Scripts/BattleStuff/BattleDataStorage.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BattleDataStorage : MonoBehaviour
+{
+    public List<CharacterHolder>[] playerCharacters = new List<CharacterHolder>[2];
+
+    public void SetCharacters(int i, CharacterHolder[] chars)
+    {
+        foreach (CharacterHolder VARIABLE in chars)
+        {
+            
+        }
+        playerCharacters[i].Add();
+    }
+}
