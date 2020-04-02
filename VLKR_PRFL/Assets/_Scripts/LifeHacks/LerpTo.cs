e02fc8507ae1d2e0700f02953f44e37893626b28
DragstylerNL <dragstylernl@gmail.com>
2020-04-02 09:54:09 +0200

    bug fixes

---
+++ VLKR_PRFL/Assets/_Scripts/LifeHacks/LerpTo.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using UnityEngine;
+
+public class LerpTo : MonoBehaviour
+{
+    public Vector3 targetPos;
+    public float speed = 0.05f;
+    public bool x, y, z;
+    private Vector3 _pos;
+
+    public void StartLerp()
+    {
+        _pos = Vector3.zero;
+        StartCoroutine(Lerp());
+    }
+
+    IEnumerator Lerp()
+    {
+        while (transform.position != targetPos)
+        {
+            Vector3 position = transform.position;
+            if(x)_pos.x = Mathf.Lerp(position.x, targetPos.x, speed);
+            if(y)_pos.y = Mathf.Lerp(position.y, targetPos.y, speed);
+            if(z)_pos.z = Mathf.Lerp(position.z, targetPos.z, speed);
+            transform.position = _pos;
+            yield return 0;
+        }
+        yield return 0;
+    }
+}
---
+++ VLKR_PRFL/Assets/_Scripts/_MainMenu/MenuManager.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEditor;
+using UnityEngine;
+
+public class MenuManager : MonoBehaviour
+{
+    [SerializeField] private GameObject mainMenu, menuDisable;
+    [SerializeField] private GameObject MP_BattleSelector, BattleDisable;
+
+    private MenuManager _mm;
+
+    private void Awake()
+    {
+        
+    }
+
+    public void StartMultiplayer()
+    {
+        menuDisable.SetActive(false);
+        mainMenu.GetComponent<LerpTo>().StartLerp();
+        BattleDisable.SetActive(true);
+        MP_BattleSelector.GetComponent<LerpTo>().StartLerp();
+    }
+}
